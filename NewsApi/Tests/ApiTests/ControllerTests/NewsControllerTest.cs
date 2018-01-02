006b7d4e8fc3925e1251213f70e1c3b4b0a4fba8
JonSteinn <jonsteinn@gmail.com>
2018-01-02 02:10:12 +0000

    some controller tests

---
+++ NewsApi/Tests/ApiTests/ControllerTests/NewsControllerTest.cs
@@ -0,0 +1,89 @@
+using System.Collections.Generic;
+using Api.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using NewsApi.Exceptions;
+using NewsApi.Models.DTOModels;
+using Tests.MockData.DTOModels;
+using Tests.MockData.Services;
+using Xunit;
+
+namespace Tests.ApiTests.ControllerTets
+{
+    public class NewsControllerTest
+    {
+
+        [Fact]
+        public void GetNews_Found_200()
+        {
+            // Arrange
+            var service = new MockNewsService{_GetNewsById = (id) => MockNewsDTO.Get(id)};
+            var controller = new NewsController(service);
+
+            // Act
+            IActionResult res = controller.GetNews(5);
+            var result = controller.GetNews(5) as OkObjectResult;
+            var newsResult = result.Value as NewsDTO;
+            
+            // Assert
+            Assert.Equal(200, result.StatusCode);
+            Assert.Equal(MockNewsDTO.Get(5), newsResult);
+        }
+
+
+        [Fact]
+        public void GetNews_Found_404()
+        {
+            // Arrange
+            var service = new MockNewsService{_GetNewsById = (id) => throw new NewsNotFoundException()};
+            var controller = new NewsController(service);
+
+            // Act
+            var result = controller.GetNews(15) as NotFoundResult;
+            
+            // Assert
+            Assert.Equal(404, result.StatusCode);
+        }
+
+        [Fact]
+        public void a()
+        {
+            // Arrange
+            var service = new MockNewsService
+            {
+                _GetAllNews = (year, month, day) => 
+                {
+                    return new List<NewsDTO>
+                    {
+                        MockNewsDTO.Get(0),
+                        MockNewsDTO.Get(3)
+                    };
+                }
+            };
+            var controller = new NewsController(service);
+
+            // Act
+            var result = controller.GetAllNews("1", "2", "3") as OkObjectResult;
+            var newsListResult = result.Value as List<NewsDTO>;
+
+            // Assert
+            Assert.Equal(2, newsListResult.Count);
+            Assert.Contains(MockNewsDTO.Get(0), newsListResult);
+            Assert.Contains(MockNewsDTO.Get(3), newsListResult);
+        }
+    }
+    
+}
+
+/*
+
+            // Arrange
+            var service = new MockNewsService{_RemoveNewsById = (id) => throw new NewsNotFoundException()};
+            var controller = new NewsController(service);
+
+            // Act
+            var result = controller.RemoveNews(5) as NotFoundResult;
+            
+            // Assert
+            Assert.Equal(404, result.StatusCode);
+
+ */
